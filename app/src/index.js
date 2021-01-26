45920859923e92a24eee4bb7305342bb363b93d7
baptistemrt <contact@baptistemrt.fr>
2021-01-26 19:20:02 +0100

    :sparkles: Import new scss

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import "./assets/scss/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
