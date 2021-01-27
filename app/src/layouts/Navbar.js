79efff1821bb360c52ed2b38b00cce9b693e1104
baptistemrt <contact@baptistemrt.fr>
2021-01-27 12:12:14 +0100

    :iphone: Add scss design for navbar buttons

--- app/src/layouts/Navbar.js
+++ app/src/layouts/Navbar.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faCircle } from "@fortawesome/free-solid-svg-icons";
-import { faDiscord, faGithub, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faDiscord, faGithub, faInstagram, faTwitch, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { NavLink } from "react-router-dom";
 
 import "../assets/scss/layouts/navbar.scss";
@@ -25,6 +25,7 @@ const Navbar = () => {
           </ul>
           <ul  className="navbar-social-list">
             <li className="navbar-social-item"><a href="https://discord.bapmarty.fr"><FontAwesomeIcon icon={faDiscord} /></a></li>
+            <li className="navbar-social-item"><a href="https://twitch.tv/mrtwizard"><FontAwesomeIcon icon={faTwitch} /></a></li>
             <li className="navbar-social-item"><a href="https://instagram.com/baptistemrrt"><FontAwesomeIcon icon={faInstagram} /></a></li>
             <li className="navbar-social-item"><a href="https://youtube.com/"><FontAwesomeIcon icon={faYoutube} /></a></li>
             <li className="navbar-social-item"><a href="https://github.com/baptistemrt"><FontAwesomeIcon icon={faGithub} /></a></li>
