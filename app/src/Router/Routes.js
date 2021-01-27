a0be9935ea077be1a7c0cf27949cbef6e2d6e576
baptistemrt <contact@baptistemrt.fr>
2021-01-27 12:40:24 +0100

    :bug: Fix redirect link

--- app/src/Router/Routes.js
+++ app/src/Router/Routes.js
@@ -5,7 +5,7 @@ import Home from "../components/Home";
 const Routes = () => (
   <Switch>
     <Route path="/live" component={Home}/>
-    <Redirect path="/live" />
+    <Redirect to="/live" />
   </Switch>
 );
 
