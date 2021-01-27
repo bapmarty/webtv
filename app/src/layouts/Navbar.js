724822fed51e909fed1bed0449db4716d25eb2a5
baptistemrt <contact@baptistemrt.fr>
2021-01-27 12:35:51 +0100

    :dizzy: Add animation on opening navbar

--- app/src/layouts/Navbar.js
+++ app/src/layouts/Navbar.js
@@ -1,10 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCircle, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { faDiscord, faGithub, faInstagram, faTwitch, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { NavLink } from "react-router-dom";
 
 import "../assets/scss/layouts/navbar.scss";
+import { useState } from "react";
 const Navbar = () => {
+
+  const [icon, setIcon] = useState(faBars);
+  const [navbarOpen, setNavbarOpen] = useState("navbar-nav");
+
+  const handleClick = () => {
+    if (icon === faBars) {
+      setIcon(faTimes);
+      setNavbarOpen("navbar-nav open");
+    } else {
+      setIcon(faBars);
+      setNavbarOpen("navbar-nav");
+    }
+  }
+
   return (
     <>
       <nav className="navbar-container">
@@ -12,11 +27,11 @@ const Navbar = () => {
           <div className="navbar-header-title">
             <h1><NavLink to="/live"><span>B</span>apmarty</NavLink></h1>
           </div>
-          <div className="navbar-header-bars">
-            <FontAwesomeIcon icon={faBars} />
+          <div className="navbar-header-bars" onClick={handleClick}>
+            <FontAwesomeIcon icon={icon} />
           </div>
         </div>
-        <div className="navbar-nav">
+        <div className={navbarOpen}>
           <ul className="navbar-nav-list">
             <li className="navbar-nav-item"><NavLink to='/live' activeClassName="active">Direct <FontAwesomeIcon icon={faCircle} /></NavLink></li>
             <li className="navbar-nav-item"><NavLink to='/prog' activeClassName="active">Programmation</NavLink></li>
