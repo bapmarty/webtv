e94ad73aab8c33c322bf50bfb578c3266229aa61
baptistemrt <contact@baptistemrt.fr>
2021-01-30 22:29:45 +0100

    :bug: Fix redirect uri link by env

--- api/Controllers/callback.controller.js
+++ api/Controllers/callback.controller.js
@@ -1,8 +1,9 @@
 const fetch = require('node-fetch');
 
-const CLIENT_ID     = process.env.TWITCH_CLIENT_ID;
-const CLIENT_SECRET = process.env.TWITCH_CLIENT_ID_SECRET;
-const REDIRECT_URI  = process.env.REDIRECT_URI + "/auth/callback";
+const CLIENT_ID         = process.env.TWITCH_CLIENT_ID;
+const CLIENT_SECRET     = process.env.TWITCH_CLIENT_ID_SECRET;
+const REDIRECT_URI      = process.env.REDIRECT_URI + "/auth/callback";
+const REDIRECT_URI_APP  = process.env.REDIRECT_URI_APP;
 
 exports.callback = (req, res) => {
   fetch(
@@ -13,7 +14,7 @@ exports.callback = (req, res) => {
     console.log("[DATA]: \n" + data);
     res.cookie('twitch_access_token', data.access_token, { expires: new Date(new Date().getTime()+21600000) });
     res.cookie('twitch_refresh_token', data.refresh_token, { expires: new Date(new Date().getTime()+21600000) });
-    res.redirect('http://localhost:3001');
+    res.redirect(REDIRECT_URI_APP);
   })
   .catch(() => res.json({error: "Error shit !"}) );
 }
