7d95ddab620a026aac874ef76136b99031604d66
baptistemrt <contact@baptistemrt.fr>
2021-02-08 13:36:12 +0100

    :sparkles: Add new component informations

--- app/src/Router/Routes.js
+++ app/src/Router/Routes.js
@@ -1,12 +1,13 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from "../components/Home";
+import Informations from '../components/Informations';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home}/>
     <Route exact path="/prog" component={Home}/>
-    <Route exact path="/info" component={Home}/>
+    <Route exact path="/info" component={Informations}/>
     <Route exact path="/rediff" component={Home}/>
     <Redirect to="/" />
   </Switch>
