862997028db7401745d33e793517c04983df7e4c
baptistemrt <contact@baptistemrt.fr>
2021-02-09 09:52:58 +0100

    :pencil2: Change text

--- app/src/components/Informations.js
+++ app/src/components/Informations.js
@@ -12,7 +12,7 @@ const Informations = () => {
         </div>
         <div className="presentation">
           <p className="text">
-            Salut, moi c'est Baptiste AKA <span className="surligned">Bapmarty</span>. J'ai 20 ans et je suis étudiant en informatique à 42 et encadrant d'escalade. 
+            Salut, moi c'est Baptiste AKA Bapmarty. J'ai 20 ans et je suis étudiant en informatique à 42 et encadrant d'escalade. 
             À travers ce site internet je souhaite te faire decouvrir un peu monde et comment je passe mon temps. 
             Tu trouveras plus bas un peu plus d'informations sur moi en escalade et en informatique mais aussi en gaming <span className="italic">(Et oui de temps en temps je joue...)</span>.
             <br /><br />Bienvenue sur ma chaîne !
