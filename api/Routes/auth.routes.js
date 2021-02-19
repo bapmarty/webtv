a57d24b16aa70b95f291e08869e3853e91c7cf05
baptistemrt <contact@baptistemrt.fr>
2021-02-19 17:09:16 +0100

    :sparkles: Add new routes for auth the administrator of the website

--- api/Routes/auth.routes.js
+++ api/Routes/auth.routes.js
@@ -1,5 +1,14 @@
+const { login, register } = require("../Controllers/admin.controller");
 const { callback } = require("../Controllers/callback.controller");
+
+const auth = require("../middlewares/auth.middleware");
 
 module.exports = app => {
   app.get('/auth/callback', callback);
+
+  // Panel routes
+  app.post('/v1/auth/admin/login', login);
+
+  // Routes using middleware auth
+  app.post('/v1/auth/admin/register', auth, register);
 }
